5b65c896162ff5e5d1b97c197b146425dfdcc8ed
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-02-25 12:20:48 +0600

    model commit

--- js/meals.js
+++ js/meals.js
@@ -21,6 +21,7 @@ const displayMeals = meals =>{
                     <div class="card-body">
                       <h5 class="card-title">${meal.strMeal}</h5>
                       <p class="card-text">This is a short card.</p>
+                      <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">Details</button>
                     </div>
                   </div>
          `
@@ -32,5 +33,19 @@ const searchMeals = () =>{
   const searchText = document.getElementById('search-field').value;
   loadMeals(searchText);
 
+}
+
+const loadMealDetail = idMeal =>{
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayMealDetails(data.meals[0]))
+}
+const displayMealDetails = meal =>{
+  document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
+  const mealsDetails = document.getElementById('mealDetailsBody');
+  mealsDetails.innerHTML = `
+  <img class="img-fluid" src="${meal.strMealThumb}" alt="">
+  `
 }
 loadMeals('chi');
